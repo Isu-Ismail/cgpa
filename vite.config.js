915ce984c237b7fc9e71a2d64b2ac8cee3035a92
Ismail <ismailisims1@gmail.com>
2026-08-30 14:33:06 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     svelte({
       configFile: './svelte.config.js'
     }),
-    tailwindcss(),
-    
+    tailwindcss()
   ]
 });
